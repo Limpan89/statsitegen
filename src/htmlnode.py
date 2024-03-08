4ba8733e9fa575cc2220cf4be25792306153d281
Limpan89 <lbroback89@gmail.com>
2024-03-08 22:22:00 +0100

    added LeafNode subclass of HTMLNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,5 @@
+
+
 class HTMLNode:
     
     def __init__(self, tag:str=None, value:str=None, children:list['HTMLNode']=None, props:dict[str,str]=None):
@@ -6,7 +8,7 @@ class HTMLNode:
         self.children = children
         self.props = props
         
-    def to_html(self):
+    def to_html(self) -> str:
         raise NotImplementedError
     
     def props_to_html(self) -> str:
@@ -26,3 +28,16 @@ class HTMLNode:
         
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+    
+class LeafNode(HTMLNode):
+    
+    def __init__(self,tag:str=None, value:str="", props:dict[str,str]=None):
+        super().__init__(tag, value, None, props)
+        
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("The value of a LeafNode must not be None")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -20,6 +20,20 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
         self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank"')
+        
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p", None)
+        node2 = LeafNode(value="This is a paragraph of text.")
+        node3 = LeafNode("p", "This is a paragraph of text.")
+        node4 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        with self.assertRaises(ValueError):
+            node.to_html()
+        self.assertEqual(node2.to_html(), "This is a paragraph of text.")
+        self.assertEqual(node3.to_html(), "<p>This is a paragraph of text.</p>")
+        self.assertEqual(node4.to_html(), '<a href="https://www.google.com">Click me!</a>')
+        
         
 
 if __name__ == "__main__":
